f7ebbe0c178cd136f3fb7d5cea0446c69ed65d40
EC2 Default User <ec2-user@ip-172-31-11-103.ap-southeast-1.compute.internal>
2019-02-05 12:09:08 +0000

    stop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length:{ maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length:{ maximum: 255 },
@@ -7,4 +10,29 @@ class User < ApplicationRecord
   
   has_secure_password
   validates :password, presence: true, length:{ minimum: 6 }, allow_nil: true #has_secure_passwordでも存在性のバリデーションが実行されるのでnilでOK
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  private
+  
+  def downcase_email
+    self.email = email.downcase
+  end
+  
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130152055) do
+ActiveRecord::Schema.define(version: 20190205101532) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20190130152055) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
