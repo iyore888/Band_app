fc7321655c7a556d297756152aa2435ea4968024
EC2 Default User <ec2-user@ip-172-31-11-103.ap-southeast-1.compute.internal>
2019-02-19 16:34:12 +0000

    modify

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ User.create!(name:  "Admin User",
                email: email,
                password:              password,
                password_confirmation: password,
+               admin: false,
                activated: true,
                activated_at: Time.zone.now)
 end
