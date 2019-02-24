5a0effa5d6ac14866e2adbb7e116059901062cab
EC2 Default User <ec2-user@ip-172-31-11-103.ap-southeast-1.compute.internal>
2019-02-24 15:50:46 +0000

    modify

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: {maximum: 140 }
   validates :user_id, presence: true
+  validates :genre, presence: true
+  validates :part, presence: true
+  validates :user_id, presence: true
+  validates :age, presence: true
+  validates :gender, presence: true
 end
