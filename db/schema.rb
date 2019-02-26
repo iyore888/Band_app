b0a77d6de1395afa11eddbc385bdeb8d6d881bff
EC2 Default User <ec2-user@ip-172-31-11-103.ap-southeast-1.compute.internal>
2019-02-26 10:38:00 +0000

    css modify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190224152951) do
+ActiveRecord::Schema.define(version: 20190225033048) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
